93af68c01a9235a7b8be0bcdda1f95a76ee27aa2
Paramveer Soundane <paramveersoundane@Paramveers-MacBook-Air.local>
2026-01-10 20:32:48 +0530

    Initial commit with patient-service code

---
+++ patient-service/src/main/java/com/pm/patientservice/service/PatientService.java
@@ -0,0 +1,32 @@
+package com.pm.patientservice.service;
+
+import com.pm.patientservice.dto.PatientRequestDTO;
+import com.pm.patientservice.dto.PatientResponseDTO;
+import com.pm.patientservice.mapper.PatientMapper;
+import com.pm.patientservice.model.Patient;
+import com.pm.patientservice.repository.PatientRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class PatientService {
+    private final PatientRepository patientRepository;
+    public PatientService(PatientRepository patientRepository){
+        this.patientRepository=patientRepository;
+    }
+
+    public List<PatientResponseDTO> getPatients(){
+        List<Patient> patients = patientRepository.findAll();
+        List<PatientResponseDTO> patientResponseDTOs=
+                patients.stream().map(PatientMapper::getDTO).toList();//patients.stream().map(patient-> PatientMapper.getDTO(patient)).toList();
+
+        return patientResponseDTOs;
+    }
+
+    public PatientResponseDTO createPatient(PatientRequestDTO patientRequestDTO){
+        Patient patient = patientRepository.save(PatientMapper.toPatient(patientRequestDTO));
+        return PatientMapper.getDTO(patient);
+
+    }
+}
