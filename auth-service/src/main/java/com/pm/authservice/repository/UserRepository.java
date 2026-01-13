a12b81f1de41b19d4cc5b98ca23d82d7182aa4b4
Paramveer Soundane <paramveersoundane@Paramveers-MacBook-Air.local>
2026-01-13 08:27:23 +0530

    Initial implementation of auth-service

---
+++ auth-service/src/main/java/com/pm/authservice/repository/UserRepository.java
@@ -0,0 +1,9 @@
+package com.pm.authservice.repository;
+
+import com.pm.authservice.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.UUID;
+
+public interface UserRepository extends JpaRepository<User, UUID> {
+}
