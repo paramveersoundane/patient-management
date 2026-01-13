a59b64fd2235ba54ca6e9b35a9309e2b18cc7e17
Paramveer Soundane <paramveersoundane@Paramveers-MacBook-Air.local>
2026-01-13 10:12:20 +0530

    validateToken code implemented for auth-service

--- auth-service/src/main/java/com/pm/authservice/util/JwtUtil.java
+++ auth-service/src/main/java/com/pm/authservice/util/JwtUtil.java
@@ -1,12 +1,15 @@
 package com.pm.authservice.util;
 
+import io.jsonwebtoken.JwtException;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.security.Keys;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
+import javax.crypto.SecretKey;
 import java.nio.charset.StandardCharsets;
 import java.security.Key;
+import java.security.SignatureException;
 import java.util.Base64;
 import java.util.Date;
 
@@ -24,5 +27,16 @@ public class JwtUtil {
                 .issuedAt(new Date())
                 .expiration(new Date(System.currentTimeMillis() + 1000*60*60*10)) //10 hours
                 .signWith(secretkey).compact();
+    }
+
+    public void validateToken(String token){
+        try{
+            Jwts.parser().verifyWith((SecretKey) secretkey)  //typecast
+                    .build().parseSignedClaims(token);
+        } catch (SignatureException e) {
+            throw new JwtException("Invalid JWT signature");
+        } catch (JwtException e) {
+            throw new JwtException("Invalid JWT token");
+        }
     }
 }
