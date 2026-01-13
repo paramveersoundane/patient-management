48e004f3fadfcba588772fa934d3213bd246237f
Paramveer Soundane <paramveersoundane@Paramveers-MacBook-Air.local>
2026-01-13 10:22:19 +0530

    http file added for validate, JwtUtil import correct for SignatureException

--- auth-service/src/main/java/com/pm/authservice/util/JwtUtil.java
+++ auth-service/src/main/java/com/pm/authservice/util/JwtUtil.java
@@ -3,40 +3,45 @@ package com.pm.authservice.util;
 import io.jsonwebtoken.JwtException;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.security.Keys;
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.stereotype.Component;
-
-import javax.crypto.SecretKey;
+import io.jsonwebtoken.security.SignatureException;
 import java.nio.charset.StandardCharsets;
 import java.security.Key;
-import java.security.SignatureException;
 import java.util.Base64;
 import java.util.Date;
+import javax.crypto.SecretKey;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
 
 @Component
 public class JwtUtil {
 
-    private final Key secretkey;
-    public JwtUtil(@Value("${jwt.secret}") String secret){
-        byte[] keyBytes = Base64.getDecoder().decode(secret.getBytes(StandardCharsets.UTF_8));
-        this.secretkey= Keys.hmacShaKeyFor(keyBytes);
+    private final Key secretKey;
+
+    public JwtUtil(@Value("${jwt.secret}") String secret) {
+        byte[] keyBytes = Base64.getDecoder()
+                .decode(secret.getBytes(StandardCharsets.UTF_8));
+        this.secretKey = Keys.hmacShaKeyFor(keyBytes);
     }
 
-    public String generateToken(String email, String role){
-        return Jwts.builder().claim("role",role)
+    public String generateToken(String email, String role) {
+        return Jwts.builder()
+                .subject(email)
+                .claim("role", role)
                 .issuedAt(new Date())
-                .expiration(new Date(System.currentTimeMillis() + 1000*60*60*10)) //10 hours
-                .signWith(secretkey).compact();
+                .expiration(new Date(System.currentTimeMillis() + 1000 * 60 * 60 *10)) // 10 hours
+                .signWith(secretKey)
+                .compact();
     }
 
-    public void validateToken(String token){
-        try{
-            Jwts.parser().verifyWith((SecretKey) secretkey)  //typecast
-                    .build().parseSignedClaims(token);
+    public void validateToken(String token) {
+        try {
+            Jwts.parser().verifyWith((SecretKey) secretKey)
+                    .build()
+                    .parseSignedClaims(token);
         } catch (SignatureException e) {
             throw new JwtException("Invalid JWT signature");
         } catch (JwtException e) {
-            throw new JwtException("Invalid JWT token");
+            throw new JwtException("Invalid JWT");
         }
     }
 }
